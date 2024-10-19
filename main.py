9c65087df9407d36a6d797e55d0f0911d409c0ad
reewqr227 <reewqr277@gmail.com>
2024-10-19 17:31:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+bot.run('YOUR_BOT_TOKEN')
